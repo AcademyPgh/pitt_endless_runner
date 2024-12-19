deb1086e1462bb47ba696ae8898d4c3233ef9c3a
John Lange <lange.john.m@gmail.com>
2024-12-18 19:02:48 -0500

    Change animation speed on active selection

--- src/scenes/select.ts
+++ src/scenes/select.ts
@@ -118,9 +118,11 @@ class StartScene extends ex.Scene {
     const activeAnimation = ex.Animation.fromSpriteSheet(
       frameSheet, 
       [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 
-      100,
+      50,
       ex.AnimationStrategy.Loop    
     );
+    activeAnimation.frames[12].duration = 400;
+
     const idleAnimation = ex.Animation.fromSpriteSheet(
       frameSheet, 
       [12], 

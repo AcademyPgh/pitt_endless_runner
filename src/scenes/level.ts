f1535d4ee3ffad0850b93bd8106092bce09f57cd
yorblesnoot <ethereos@gmail.com>
2025-01-18 13:39:27 -0500

    added fade in dream image to game over and blinking for initials buttons

--- src/scenes/level.ts
+++ src/scenes/level.ts
@@ -6,6 +6,7 @@ import { select } from './select';
 import { Resources } from '../resources';
 import InputManager from '../utils/input';
 import { Game } from '../main';
+import * as ex from 'excalibur';
 
 class Level extends Scene {
   private baseSpeed = 150;
@@ -51,8 +52,15 @@ class Level extends Scene {
   }
 
   gameover(engine: ex.Engine){
-    engine.goToScene('end');
+    let height = engine.canvasHeight
+    let width = engine.canvasWidth
+    let screen = new ex.Actor({height, width, z: 100, x: width/2, y: height/2})
+    let sprite = new ex.Sprite({image: Resources.ui.dream, destSize: {height, width}})
+    screen.graphics.use(sprite)
+    screen.graphics.opacity = 0
+    this.add(screen)
     Resources.sounds.music.pause()
+    screen.actions.fade(1, 1000).delay(3000).callMethod(() => engine.goToScene('end'))
   }
 }
 
